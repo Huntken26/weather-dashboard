f75d728539b9983f5941c375229b126e115cf85a
huntken26 <huntken26@gmail.com>
2022-10-04 02:52:22 -0500

    started my js functions, jQuery ajax, on click event, and set variables

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,4 +1,36 @@
-// jQuery to setup local storage for the searched cities
-//Momentjs for thew current date and time, convert it to the weather api?
-// Ajax to get the data from OpenWeather API and target/update each section
+// Setup variables and query selectors
+//On click event for search button
+//jQuery to setup local storage for the searched cities
+//Momentjs for the current date and time, convert it to the weather api?
+// Ajax or fetch to get the data from OpenWeather API and target/update each section
 //funcitons- search button; stringify array for total cities searched; display 5 day weather stats etc
+//call the functions at the bottom 
+
+function weatherFunction() {
+  var currentDate = "";
+  var currentCity = "";
+  var searchInput;
+  var formInput;
+  var queryString;
+
+$.ajax({
+    url: requestUrl,
+    method: 'GET',
+  }).then(function (response) {
+    console.log('AJAX Response \n-------------');
+    console.log(response);
+  });
+
+  var timeNow = moment();
+  $("#currentDay").text(timeNow.format("LLLL"));
+
+  $(".btn").on("click", function citySearch(event) {
+    event.preventDefault();
+    location.assign();
+    localStorage.setItem();
+  });
+
+  $().val(localStorage.getItem());
+}
+
+weatherFunction();

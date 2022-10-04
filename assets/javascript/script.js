ad4e8e4d43b4e05ab045cf93d7eabbd137ccdc59
huntken26 <huntken26@gmail.com>
2022-10-04 01:47:29 -0500

    Fixed the html parents/siblings, CSS styling

---
+++ assets/javascript/script.js
@@ -0,0 +1,4 @@
+// jQuery to setup local storage for the searched cities
+//Momentjs for thew current date and time, convert it to the weather api?
+// Ajax to get the data from OpenWeather API and target/update each section
+//funcitons- search button; stringify array for total cities searched; display 5 day weather stats etc

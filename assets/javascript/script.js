6946a99cf6d3df3b99441dbcb63c63d29f3bd7d6
huntken26 <huntken26@gmail.com>
2022-10-06 15:41:06 -0500

    Added some javascript

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,20 +4,20 @@
 // example string =   "http://api.openweathermap.org/geo/1.0/direct?q=" + cityInput + "&appid=" + APIKey;
 var locateButton = document.getElementById("find-me");
 var city = document.getElementById("list1");
-
+var searchButton = document.getElementById("search-button");
 
 
 //Created a function to capture the geolocation data from the user's browser and use that for the openweather api
-  function geoLocate() {
+  function locateApi() {
     var status = document.querySelector('#status');
     var locationData = document.querySelector('#location-data');
     locationData.textContent = '';
 
     function success(position) {
-      var latitude  = position.coords.latitude;
-      var longitude = position.coords.longitude;
-      var APIKey = "f54d50bfb2e404deefe09cc2818a598f";
-      var queryString = "https://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&units=imperial" + "&appid=" + APIKey;
+      let latitude  = position.coords.latitude;
+      let longitude = position.coords.longitude;
+      let APIKey = "f54d50bfb2e404deefe09cc2818a598f";
+      let queryString = "https://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&units=imperial" + "&appid=" + APIKey;
   
       status.textContent = '';
       locationData.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
@@ -44,8 +44,40 @@ var city = document.getElementById("list1");
     
       }
   
- locateButton.addEventListener('click', geoLocate);
+ locateButton.addEventListener('click', locateApi);
 
+
+ function searchApi() {
+    let input = document.getElementById("search-input").value;
+    let APIKey = "f54d50bfb2e404deefe09cc2818a598f";
+    let searchString = "http://api.openweathermap.org/geo/1.0/direct?q=" + input + "&appid=" + APIKey;
+
+    fetch(searchString)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
+
+    // function successSearch() {
+    //   let latitude  = position.coords.latitude;
+    //   let longitude = position.coords.longitude;
+    //   let APIKey = "f54d50bfb2e404deefe09cc2818a598f";
+    //   let newString = "https://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&units=imperial" + "&appid=" + APIKey;
+  
+    //   fetch(newString)
+    //   .then(function (response) {
+    //     return response.json();
+    //   })
+    //   .then(function (data) {
+    //     console.log(data);
+    //   });
+  
+    // }
+  }
+
+searchButton.addEventListener('click', searchApi);
 
 //   Latitude /lngitude 
 // Atlanta 33.748997 / -84.387985
